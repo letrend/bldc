b298d16e4d6b17a5fb21e90c85e87f0a1531eb22
letrend <simon.trendel@tum.de>
2017-02-14 23:10:03 +0100

    with draft cross-correlation

--- applications/app_longshoard.c
+++ applications/app_longshoard.c
@@ -6,6 +6,7 @@
 #include "hw.h" // Pin mapping on this hardware
 #include "timeout.h" // To reset the timeout
 #include "crc.h"
+#include "digital_filter.h"
 #include <string.h>
 
 // Settings
@@ -47,14 +48,65 @@ enum LONGSHOARD_COMMAND{
     START_DATA = 52
 };
 
+#define CIRCULAR_BUFFER_LENGTH 128
+
+typedef struct _CircularBuffert{
+    uint8_t     mWrite;
+    float       mBuffer[CIRCULAR_BUFFER_LENGTH];
+}CircularBuffert;
+
+#define CIRCBUF_write(cirbuf, data, size)    {                                 \
+    uint8_t temp = ((cirbuf.mWrite +1 )& (size -1));                           \
+    cirbuf.mBuffer[temp] = data;                                                 \
+    cirbuf.mWrite = temp;                                                        \
+}
+
 float duty = 0, brake_current = 0, min_weight = 0.3f, max_weight = 0.1f;
 bool brake_now = false, release_motor = true;
-bool mode[] = {false, false, false, false};
+bool mode[4];
 unsigned int data_point = 0;
 unsigned short checksum = 0;
-float rpm_data[200], weight_data[200], current_data[200];
+CircularBuffert rpm_measure, weight_measure, current_measure;
+static float rpm_data[CIRCULAR_BUFFER_LENGTH], weight_data[CIRCULAR_BUFFER_LENGTH], current_data[CIRCULAR_BUFFER_LENGTH];
+float std_rpm = 0, std_weight = 0, std_current = 0;
 bool data_valid = false, receiving_data = false;
+uint32_t total_number_of_data_points = 0;
 bool manual_control = true;
+
+float cross_correlate(CircularBuffert* signal, float* filter, float std_filter, float signal_mean){
+  int j = signal->mWrite;
+  float response = 0, std = 0;
+  for(int i=0; i<total_number_of_data_points; i++){
+      std+=(signal->mBuffer[j]-signal_mean)*(signal->mBuffer[j]-signal_mean);
+      j--;
+      if(j==0){
+        j = CIRCULAR_BUFFER_LENGTH-1;
+      }
+  }
+  std = sqrtf(std);
+  j = signal->mWrite;
+  for(int i=total_number_of_data_points-1; i>=0; i--){
+    response += (filter[i]*(signal->mBuffer[j]-signal_mean));
+    j--;
+    if(j==0){
+      j = CIRCULAR_BUFFER_LENGTH-1;
+    }
+  }
+  return response/(std*std_filter);
+}
+
+float normalize(float *filter, int len){
+  float mean = 0, std = 0;
+  for(int i=0; i<len; i++){
+    mean += filter[i];
+  }
+  mean/=len;
+  for(int i=0; i<len; i++){
+    std+=(filter[i]-mean)*(filter[i]-mean);
+    filter[i]-=mean;
+  }
+  return sqrtf(std);
+}
 
 uint64_t pack754(long double f, unsigned bits, unsigned expbits)
 {
@@ -194,13 +246,14 @@ static void send_packet_wrapper(unsigned char *data, unsigned int len) {
 
 bool checkIntegrityAndMerge(float *des, unsigned char* data, unsigned int len ){
   checksum = crc16(&data[4], len-4);
-  unsigned short frame_checksum = (unsigned short) (data[2] << 8) | data[1];
+  unsigned short frame_checksum = (unsigned short) ((data[2] << 8) | data[1]);
   bool valid = false;
   if(checksum==frame_checksum){ // check if data is not corrupted
     valid = true;
     unsigned int i = 0;
-    for(i = 4; i<data[3]*sizeof(uint32_t); i+=sizeof(uint32_t)){
-      uint32_t fi = (uint32_t) (data[i+4] << 24| data[i+3] << 16 |data[i+2] << 8| data[i+1]);
+    int number_of_samples = data[3];
+    for(i = 4; i<number_of_samples*sizeof(uint32_t); i+=sizeof(uint32_t)){
+      uint32_t fi = (uint32_t) (data[i+3] << 24| data[i+2] << 16 |data[i+1] << 8| data[i]);
       des[data_point++] = unpack754_32(fi);
     }
   }
@@ -272,11 +325,18 @@ static void longshoard_commands_process_packet(unsigned char *data, unsigned int
       }
       case END_DATA:{
         receiving_data = false;
-        uint32_t total_number_of_data_points = (uint32_t) (data[4] << 24| data[3] << 16 |data[2] << 8| data[1]);
+        total_number_of_data_points = (uint32_t) (data[5] << 24| data[4] << 16 |data[3] << 8| data[2]);
         if(data_point==total_number_of_data_points && data_valid){ // if data valid so far and the number of data points is correct
           data_valid = true;
+          if(data[1]==RPM_DATA){
+            std_rpm = normalize(rpm_data,total_number_of_data_points);
+          }else if(data[1]==WEIGHT_DATA){
+            std_weight = normalize(weight_data,total_number_of_data_points);
+          }else if(data[1]==CURRENT_DATA){
+            std_current = normalize(current_data,total_number_of_data_points);
+          }
         }else{
-          data_valid = true;
+          data_valid = false;
         }
         break;
       }
@@ -340,41 +400,65 @@ static THD_FUNCTION(longshoard_thread, arg) {
 
 	chRegSetThreadName("APP_LONGSHOARD");
 
+  uint32_t count = 0;
+  rpm_measure.mWrite = 0;
+  current_measure.mWrite = 0;
+  weight_measure.mWrite = 0;
+
+  float rpm_mean = 0, weight_mean = 0, current_mean = 0;
+
+  long int iter = 0;
+
 	for(;;) {
     // Read the pot value and scale it to a number between 0 and 1 (see hw_46.h)
 		float pot = (float)ADC_Value[ADC_IND_EXT];
 		pot /= 4095.0;
 
+    float values[5];
+
+    values[0] = mc_interface_get_rpm();
+    values[1] = mc_interface_read_reset_avg_motor_current();
+    values[2] = GET_INPUT_VOLTAGE();
+    values[3] = pot;
+
     if(!manual_control){
-      if(pot >= max_weight){
-        mc_interface_release_motor();
-      }else{
-        float rpm = mc_interface_get_rpm();
-        mc_interface_set_pid_speed(rpm);
+      if(mode[0]){
+        values[4] = cross_correlate(&rpm_measure,rpm_data,std_rpm,rpm_mean);
+      }else if(mode[1]){
+        values[4] = cross_correlate(&weight_measure,weight_data,std_weight,weight_mean);
+      }else if(mode[2]){
+        values[4] = cross_correlate(&current_measure,current_data,std_current,current_mean);
       }
+    }else{
+      values[4] = rpm_mean;
     }
 
-		float values[4];
+    rpm_mean = (rpm_mean + values[0])/(float)iter;
+    weight_mean = (weight_mean + values[3])/(float)iter;
+    current_mean = (current_mean + values[1])/(float)iter;
 
-		values[0] = mc_interface_get_rpm();
-		values[1] = mc_interface_read_reset_avg_motor_current();
-		values[2] = GET_INPUT_VOLTAGE();
-		values[3] = pot;
+    CIRCBUF_write(rpm_measure,values[0],CIRCULAR_BUFFER_LENGTH);
+    CIRCBUF_write(current_measure,values[1],CIRCULAR_BUFFER_LENGTH);
+    CIRCBUF_write(weight_measure,values[3],CIRCULAR_BUFFER_LENGTH);
 
-		uint32_t fi[5];
-		fi[0] = (receiving_data | data_valid << 1 | manual_control << 2);
+		uint32_t fi[6];
+		fi[0] = (receiving_data | data_valid << 1 | manual_control << 2 |
+      mode[0] << 3 | mode[1] << 4 | mode[2] << 5 | mode[3] << 6);
 		fi[1] = pack754_32(values[0]);
 		fi[2] = pack754_32(values[1]);
 		fi[3] = pack754_32(values[2]);
 		fi[4] = pack754_32(values[3]);
+    fi[5] = pack754_32(values[4]);
 
-	 	packet_send_packet((unsigned char*)&fi, sizeof(uint32_t)*5, PACKET_HANDLER);
+	 	packet_send_packet((unsigned char*)&fi, sizeof(uint32_t)*6, PACKET_HANDLER);
 
-		commands_printf("%f\n",(double)pot);
+    // commands_printf("%d\n",total_number_of_data_points);
 
 		chThdSleepMilliseconds(50);
 
 		// Reset the timeout
 		timeout_reset();
+
+    iter++;
 	}
 }
